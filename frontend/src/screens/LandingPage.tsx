4e9b8c0dad4c20cf0c7fc83e1cd8a4937bf0de1d
DerekDao01 <dddao1@uci.edu>
2025-10-01 22:53:11 -0700

    add nav bar to top of page

--- frontend/src/screens/LandingPage.tsx
+++ frontend/src/screens/LandingPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProfilePic from "../components/ProfilePic"
+import TopBar from "../components/TopBar"
 import profileStyles from "../components/ProfilePic.module.css";
 import styles from "./LandingPage.module.css"; 
 
@@ -7,6 +8,7 @@ import styles from "./LandingPage.module.css";
 function LandingPage() {
     return (
       <div>
+        <TopBar></TopBar>
         <ProfilePic className={profileStyles.ProfilePic} />
         <h1 className={styles.message}>
           Hello! I'm <span className={styles.name}>Derek Dao</span>. You can find some of the work I've done here.
