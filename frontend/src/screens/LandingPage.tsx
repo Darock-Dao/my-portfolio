ccf9c1f37fb68f5fef81ecc80656fe03ab323714
DerekDao01 <dddao1@uci.edu>
2025-10-02 18:30:03 -0700

    start creating about me section

--- frontend/src/screens/LandingPage.tsx
+++ frontend/src/screens/LandingPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProfilePic from "../components/ProfilePic"
 import TopBar from "../components/TopBar"
+import AboutMe from "../components/AboutMe"
 import profileStyles from "../components/ProfilePic.module.css";
 import styles from "./LandingPage.module.css"; 
 
@@ -14,6 +15,7 @@ function LandingPage() {
           Hello! I'm <span className={styles.name}>Derek Dao</span>. You can find some of the work I've done here.
           
         </h1>
+        <AboutMe></AboutMe>
       </div>
     );
   }
