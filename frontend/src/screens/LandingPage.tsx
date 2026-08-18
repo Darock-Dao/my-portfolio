b7797f1c2ca6cae5d794ffb2b257f6b0f1d9b01d
DerekDao01 <dddao1@uci.edu>
2026-08-17 23:50:00 -0700

    make blank skeletons for Experience and Contact sections

--- frontend/src/screens/LandingPage.tsx
+++ frontend/src/screens/LandingPage.tsx
@@ -1,7 +1,9 @@
 import TopBar from "../components/TopBar"
 import Home from "../components/Home"
 import AboutMe from "../components/AboutMe"
+import Experience from "../components/Experience"
 import Projects from "../components/Projects"
+import Contact from "../components/Contact"
 
 function LandingPage() {
     return (
@@ -16,8 +18,16 @@ function LandingPage() {
           <AboutMe/>
         </section>
 
+        <section id = "experience" className = "experienceSection">
+          <Experience/>
+        </section>
+
         <section id = "projects" className = "projectSection">
           <Projects/>
+        </section>
+
+        <section id = "contact" className = "contactSection">
+          <Contact/>
         </section>
 
       </div>
