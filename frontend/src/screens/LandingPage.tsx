275a48cc27cf1619a79c1f3f9e9b490b439e2108
DerekDao01 <dddao1@uci.edu>
2026-08-17 22:43:04 -0700

    make home its own module

--- frontend/src/screens/LandingPage.tsx
+++ frontend/src/screens/LandingPage.tsx
@@ -1,28 +1,16 @@
-import ProfilePic from "../components/ProfilePic"
 import TopBar from "../components/TopBar"
+import Home from "../components/Home"
 import AboutMe from "../components/AboutMe"
 import Projects from "../components/Projects"
-import SocialLinks from "../components/SocialLinks";
-import profileStyles from "../components/ProfilePic.module.css";
-import styles from "./LandingPage.module.css"; 
 
 function LandingPage() {
     return (
       <div>
         <TopBar/>
 
-        <div id = "home"></div>
-
-        <ProfilePic className={profileStyles.ProfilePic} />
-        <h1 className={styles.title}>
-        Derek Dao | Software Developer
-        </h1>
-
-        <SocialLinks />
-
-        <h2 className={styles.message}>
-        Hello! I'm <span className={styles.name}>Derek Dao</span>. You can find some of the work I've done here.
-        </h2>
+        <section id = "home" className = "homeSection">
+          <Home/>
+        </section>
 
         <section id = "about" className = "aboutSection">
           <AboutMe/>
